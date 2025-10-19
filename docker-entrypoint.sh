b14ab7f9bf64d5b18596f732e0a04c296b2e0f00
Matej Pakán <matej.pakan@igtconsulting.eu>
2025-10-19 21:19:11 +0200

    Add installation of Composer and npm dependencies in entrypoint script

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,6 +1,20 @@
 #!/bin/sh
 set -e
 
+# Install dependencies if vendor directory doesn't exist (due to volume mount)
+if [ ! -d "vendor" ]; then
+    echo "Installing Composer dependencies..."
+    composer install --optimize-autoloader --no-dev
+fi
+
+# Install node dependencies and build assets if node_modules doesn't exist
+if [ ! -d "node_modules" ]; then
+    echo "Installing npm dependencies..."
+    npm install
+    echo "Building assets..."
+    npm run build
+fi
+
 # Generate APP_KEY if it doesn't exist
 if [ ! -f .env ]; then
     echo "Creating .env file..."
